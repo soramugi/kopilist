1bad6fd3971691efe25d99fbad90262879eb27ae
soramugi <soramugi.chika@gmail.com>
2013-12-29 00:18:53 +0900

    Drop tbl_login

---
+++ protected/migrations/m131228_151340_drop_login.php
@@ -0,0 +1,20 @@
+<?php
+
+class m131228_151340_drop_login extends CDbMigration
+{
+	public function safeUp()
+	{
+		$this->dropTable('tbl_login');
+	}
+
+	public function safeDown()
+	{
+		$this->createTable('tbl_login', array(
+			'id' => 'pk',
+			'user_id' => 'int NOT NULL',
+			'hash' => 'string NOT NULL',
+			'type' => 'string',
+			'create_time' => 'datetime',
+		));
+	}
+}
