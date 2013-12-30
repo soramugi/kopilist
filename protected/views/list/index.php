27f3ca10757bfd52ef8ed5c2cc39714fd4f4bba2
soramugi <soramugi.chika@gmail.com>
2013-12-31 03:08:01 +0900

    List archive the Check

--- protected/views/list/index.php
+++ protected/views/list/index.php
@@ -7,9 +7,20 @@ $this->breadcrumbs=array(
 ?>
 
 <?php foreach($models as $_model): ?>
-<p><?php echo CHtml::encode($_model->text) ?></p>
+	<p>
+		<?php echo CHtml::checkbox('check_list', false, array(
+			'id'=>"cb{$_model->id}",
+			'ajax'=>array(
+				'type'=>'POST',
+				'url'=>'check',
+				'data'=>array('pk'=>$_model->id),
+				'success'=>"function(data){
+					var cb=$('#cb{$_model->id}');
+					cb.attr('checked', !cb.is(':checked'));
+				}",
+			))); ?>
+		<?php echo CHtml::encode($_model->text) ?>
+	</p>
 <?php endforeach ?>
 
-<?php $this->renderPartial('/list/_form',array(
-	'model'=>$model,
-)); ?>
+<?php $this->renderPartial('/list/_form',array('model'=>$model,)); ?>
